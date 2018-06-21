9afd3c61a3d01f7b750a1414ab0c1332fdf556ed
Roman Mueller <roman.mueller@gmail.com>
2018-06-21 15:56:51 +0200

    Add BitBar "ಠ_ಠ" plugin

---
+++ bitbar/disapproval.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# <bitbar.title>ಠ_ಠ</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author.github>roman-mueller</bitbar.author.github>
+# <bitbar.desc>ಠ_ಠ</bitbar.desc>
+
+if [ "$1" = "copy" ]; then
+  echo -n "ಠ_ಠ" | LANG=en_US.UTF-8 pbcopy
+  exit 0
+fi
+
+echo 'ಠ_ಠ'
+echo "---"
+echo "Copy ಠ_ಠ | terminal=false bash='$0' param1=copy"
