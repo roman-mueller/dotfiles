b593c54c7b4b50aa3f34c7c3d78cd3a877715cd8
Roman Mueller <roman.mueller@gmail.com>
2018-12-05 15:51:42 +0100

    Add nmap

--- bootstrap/brew.sh
+++ bootstrap/brew.sh
@@ -5,6 +5,7 @@
 brew install pwgen
 brew install wget
 brew install mtr
+brew install nmap 
 # macOS base64 uses different cases for options than coreutils version.
 # Use from brew instead:
 brew install base64
