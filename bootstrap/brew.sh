2de9a0936d777e68e17e765ea4e445e98eab3b7b
Roman Mueller <roman.mueller@gmail.com>
2020-10-10 15:36:00 +0200

    Add watch

--- bootstrap/brew.sh
+++ bootstrap/brew.sh
@@ -6,6 +6,7 @@ brew install pwgen
 brew install wget
 brew install mtr
 brew install nmap 
+brew install watch
 # macOS base64 uses different cases for options than coreutils version.
 # Use from brew instead:
 brew install base64
