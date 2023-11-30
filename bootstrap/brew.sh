f6d93e25ba3f5a9daac1031a3164e062c989e482
Roman Mueller <roman.mueller@rm-it.de>
2023-11-30 09:37:25 +0100

    add more pip/brew tools

--- bootstrap/brew.sh
+++ bootstrap/brew.sh
@@ -2,6 +2,7 @@
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew install binwalk
 brew install pwgen
 brew install wget
 brew install mtr
@@ -11,6 +12,9 @@ brew install htop
 brew install p7zip
 brew install unrar
 brew install xz
+brew install lzop
+brew install squashfs
+brew install john-jumbo
 # macOS base64 uses different cases for options than coreutils version.
 # Use from brew instead:
 brew install base64
